237d88d3ab04d4f32486718bc5e91e17313cdba5
zilionn <142235593+zilionn@users.noreply.github.com>
2023-08-14 12:30:06 -0300

    Update MenorValorController.java
    Erro enquanto anexava os arquivos - controller veio vazio.

--- src/controller/MenorValorController.java
+++ src/controller/MenorValorController.java
@@ -3,7 +3,19 @@ package controller;
 public class MenorValorController {
 
 	public MenorValorController() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
-
+	public int menorValor(int[] vet, int tamanho, int menor) {
+		// condição de parada - quando o tamanho do vetor for <= a 0;
+		// e o menor valor será reconhecido quando todo o vetor já estiver percorrido por inteiro. 
+		if (tamanho <= 0) {
+			return menor;
+		}
+		if (vet[tamanho - 1] < menor) {
+			menor = vet[tamanho - 1];
+		}
+		// relação de chamada dos passos - checar o se o valor é menor que o valor em questão;
+		// X - 1 < Y? e logo após chamar a função com o valor sendo retirado um a um = isso irá encontrar o menor valor.
+		return menorValor(vet, (tamanho - 1), menor);
+	}
 }
